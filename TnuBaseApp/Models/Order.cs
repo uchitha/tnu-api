53fb088a54f4b09c900e9dcddb745e5190a4e027
uchitha <uchitha.r@gmail.com>
2013-06-11 10:28:23 +0800

    Update Order.cs

--- TnuBaseApp/Models/Order.cs
+++ TnuBaseApp/Models/Order.cs
@@ -7,9 +7,31 @@ namespace TnuBaseApp.Models
 {  
     public class Order
     {
+        public Order()
+        {
+            ItemQuantityList = CreateList(10);
+        }
+
         public int NumberOfItems { get; set; }
+        public List<SelectListItem> ItemQuantityList { get; set; }
         public string CustomerName { get; set; }
         public string CustomerPhoneNumber { get; set; }
         public string CustomerAddress { get; set; }
+
+        public string OrderStatus { get; set; }
+
+        private List<SelectListItem> CreateList(int max)
+        {
+            var list = new List<SelectListItem>();
+            for (int i = 1; i <= max; i++)
+            {
+                var item = new SelectListItem();
+                item.Text = i.ToString();
+                item.Value = i.ToString();
+                list.Add(item);
+            }
+
+            return list;
+        }
     }
 }
